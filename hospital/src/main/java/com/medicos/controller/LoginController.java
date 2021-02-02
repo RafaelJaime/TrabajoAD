988452845a0819050a8ada5730cd2562dcdeb4f8
AlejandroCamero <72436513+AlejandroCamero@users.noreply.github.com>
2021-02-02 08:27:54 +0100

    wd

---
+++ hospital/src/main/java/com/medicos/controller/LoginController.java
@@ -0,0 +1,58 @@
+package com.medicos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.medicos.model.User;
+import com.medicos.service.UserService;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	@Qualifier("userService")
+	private UserService userService;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@GetMapping("/auth/login")
+	public String login(Model model, @RequestParam(name="error", required=false)String error, @RequestParam(name="logout",required=false) String logout) {
+		
+		model.addAttribute("user",new User());
+		model.addAttribute("error",error);
+		model.addAttribute("logout",logout);
+		
+		return "User/login";
+	}
+	
+	@GetMapping("/login-post")
+	public String loginPost() {
+		return "redirect:/patient/list";
+	}
+	
+	@PostMapping("/auth/register")
+	public String register(@ModelAttribute User user, RedirectAttributes flash) {
+		userService.register(user);
+		flash.addFlashAttribute("Success","User registered successfully!");
+		return "redirect:/auth/login";
+	}
+	
+	@GetMapping("/auth/registerForm")
+	public String registerForm(Model model) {
+		model.addAttribute("user", new User());
+		return "User/register";
+	}
+
+
+
+}
--- hospital/src/main/java/com/medicos/controller/MainController.java
+++ hospital/src/main/java/com/medicos/controller/MainController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/paco")
+@RequestMapping("")
 public class MainController {
-	@GetMapping("/")
+	@GetMapping("")
 	public String welcome() {
 		return "base";
-	}AQUIAQUI
+	}
 }
