03ba4b93a4b594274ac105825a021c743adb0131
AlejandroCamero <72436513+AlejandroCamero@users.noreply.github.com>
2021-02-09 10:41:53 +0100

    Ya va el login

--- hospital/src/main/java/com/medicos/model/User.java
+++ hospital/src/main/java/com/medicos/model/User.java
@@ -1,86 +1,52 @@
 package com.medicos.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Entity
-@Table(name="user")
 public class User {
 	
 	@Id
-	@GeneratedValue
-	private long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 	
-	@Column(name="username",unique=true,nullable=false)
-	private String username;
 	
-	@Column(name="password",nullable=false)
+	private String name;
+	
+	
 	private String password;
 	
-	private boolean enabled;
 	
-	private String role;
-
-	public User(long id, String username, String password, boolean enabled, String role) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.enabled = enabled;
-		this.role = role;
-	}
-
-	public User() {
-		super();
-	}
-
-	public long getId() {
+	public int getId() {
 		return id;
 	}
-
-	public void setId(long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
-
-	public String getUsername() {
-		return username;
+	public String getName() {
+		return name;
 	}
-
-	public void setUsername(String username) {
-		this.username = username;
+	public void setName(String name) {
+		this.name = name;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	public boolean isEnabled() {
-		return enabled;
+	public User() {
+		super();
 	}
-
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
+	public User(int id, String name, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
 	}
+	
+	
 
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	public static UserBuilder withUsername(String username2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
--- hospital/src/test/java/com/medicos/HospitalApplicationTests.java
+++ hospital/src/test/java/com/medicos/HospitalApplicationTests.java
@@ -1,13 +1,29 @@
 package com.medicos;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.medicos.interfaces.IUser;
+import com.medicos.model.User;
 
 @SpringBootTest
 class HospitalApplicationTests {
 
+	@Autowired
+	private IUser repository;
+	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
 	@Test
-	void contextLoads() {
+	void CreateUser() {
+		User us=new User();
+		us.setId(1);
+		us.setName("Ale");
+		us.setPassword(encoder.encode("123"));
+		User newUser = repository.save(us);
 	}
 
 }
